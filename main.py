17cb270551318aa4babba760bc58e293be193fb2
N_XY <n.kubliy@gmail.com>
2020-10-29 13:44:07 +0200

    fix logic of geting orders_sequence

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from datetime import datetime
+import json
 import sqlite3 as sq
 
 app = Flask(__name__)
@@ -11,7 +12,7 @@ db = sq.connect('../PBot/bot.db', check_same_thread=False)
 class Server(Resource):
     def get(self):
         # return {'data': str(db.cursor().execute("SELECT * FROM goods").fetchall())}
-        return {'data': "Podushechnaya Server"}
+        return {'data': "Podushechnaya API"}
 
 
 class GetCurrentOrder(Resource):
@@ -23,16 +24,28 @@ class GetCurrentOrder(Resource):
         return {'data': data}
 
 
-class GetHistory(Resource):
+class GetOrdersSequence(Resource):
     def get(self):
-        data = db.cursor().execute('SELECT time FROM history')
+        data = db.cursor().execute('SELECT * FROM history').fetchall()
+        goods = db.cursor().execute('SELECT gid, name FROM goods').fetchall()
+        goods_dict = dict()
+        print(data)
+        for i in goods:
+            goods_dict[str(i[0])] = i[1]
+        new_data = []
+        print(goods_dict)
+        a = json.loads(data[0][2])
+
+        for i in a:
+            new_data.append([goods_dict[str(i[0])], i[1]])
+        data = (data[0][0], data[0][1], new_data)
 
         return {'data': data}
 
 
 api.add_resource(Server, '/')
 api.add_resource(GetCurrentOrder, '/get_current_order/')
-api.add_resource(GetHistory, '/get_history')
+api.add_resource(GetOrdersSequence, '/get_orders_sequence')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='80')
